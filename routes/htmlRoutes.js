f4f82d7ef903a94a99478c8a39f0387f62e386a2
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-16 11:26:36 -0400

    Attempt to fix the errors in htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
