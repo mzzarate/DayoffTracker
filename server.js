65c5e8468f9adcf12b38bbafb6a01372cf7c58a8
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 09:28:32 -0400

    Added find user data to server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var db = require('./models');
 
 var app = express();
 
+var passport = require('passport');
+
+var passportGoogleAuth = require('passport-google-oauth20');
+
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 var PORT = process.env.PORT || 3000;
@@ -14,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes');
+require('./routes/apiRoutes')(app,passport);
+require('./routes/htmlRoutes')(app);
 
 var syncOptions = { force: false };
 
@@ -25,8 +29,8 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 };
 
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log('App listening on: http://localhost:' + PORT);
   });
 });
@@ -36,12 +40,44 @@ passport.use(new GoogleStrategy({
   clientSecret: GOOGLE_CLIENT_SECRET,
   callbackURL: "http://www.example.com/auth/google/callback"
 },
-function(accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    return cb(err, user);
-  });
-}
+  function (accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
 ));
+
+
+//try to find user
+db.User.findOne({
+  google_id: google_id
+})
+  .then(function (user) {
+    if (!user) {
+      // SAVE USER DATA HERE
+      db.User.create({
+        email: email,
+        google_id: google_id
+      })
+        .then(function (user) {
+          //more magic after creating the user
+          return cb(null, user);
+        });
+    }
+    else {
+      //more magic after finding the user
+      return cb(null, user);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    return cb(err, null);
+  });
+
+// when we save a user to a session
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
 
 
 module.exports = app;
