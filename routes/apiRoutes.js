42605b3203323f3f671bd6cb00e8a22993e048d1
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 10:42:27 -0400

    Attempt to fix double qoute error on line 31 of apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,7 +28,7 @@ module.exports = function(app) {
     passport.authenticate("google", { scope: ["profile"] }));
 
   app.get("/auth/google/callback",
-    passport.authenticate('google', { failureRedirect: '/login' }),
+    passport.authenticate("google", { failureRedirect: "/login" }),
     function (req, res) {
       // Successful authentication, redirect home.
       res.redirect('/');
