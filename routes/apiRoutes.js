d5cbcb0be4c85e87cca0563daf2961bb58bb0518
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 10:56:55 -0400

    Attempt to fix insert error on line 27 of apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,8 +25,8 @@ module.exports = function(app) {
   });
 
   app.get("/auth/google",
-    passport.authenticate("google", { scope: ["profile"] 
-  }));
+    passport.authenticate("google", { scope: ["profile"] })
+  );
 
   app.get("/auth/google/callback",
     passport.authenticate("google", { failureRedirect: "/login" }),
