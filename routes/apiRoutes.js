55d61a87cf34decd48ab93b2182dca250714cef3
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 12:16:32 -0400

    Attempt to fix errors on line 32 of apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,7 +29,8 @@ module.exports = function(app) {
     passport.authenticate("google", { scope: ["profile"] })
   );
 
-  app.get("/auth/google/callback",
+  app.get(
+    "/auth/google/callback",
     passport.authenticate("google", { failureRedirect: "/login" }),
     function(req, res) {
       // Successful authentication, redirect home.
