32c10f34e3119e39fc52746d7b0be5acb38775ad
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 11:00:58 -0400

    Attempt to fix Insert error on line 31 of apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,11 +25,11 @@ module.exports = function(app) {
   });
 
   app.get("/auth/google",
-    passport.authenticate("google", { scope: ["profile"] })
-  );
+    passport.authenticate("google", { scope: ["profile"] }));
 
   app.get("/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/login" }),
+    passport.authenticate("google", { failureRedirect: "/login" }
+    ),
     function(req, res) {
       // Successful authentication, redirect home.
       res.redirect("/");
