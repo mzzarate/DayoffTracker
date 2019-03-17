2549906632479f426156e63d81e50d4edbfd202f
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 11:06:38 -0400

    Attempt to fix errors on line 27 of apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,8 +24,8 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/auth/google",
-    passport.authenticate("google", { scope: ["profile"] }));
+  app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] })
+  );
 
   app.get("/auth/google/callback",
     passport.authenticate("google", { failureRedirect: "/login" }),
