3c12e447fefeec116429fb0535bfedb25cff904d
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-17 10:35:19 -0400

    Attempt to fix double quote error on line

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
   });
 
   app.get("/auth/google",
-    passport.authenticate('google', { scope: ['profile'] }));
+    passport.authenticate("google", { scope: ['profile'] }));
 
   app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
