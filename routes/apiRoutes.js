357b443406514dae633820ee3436fe9f7060dc89
git config --global user.name TNorsang <thenorsang@gmail.com>
2019-03-21 02:33:30 -0400

    added models

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,33 @@
 var db = require("../models");
 
 module.exports = function(app, passport) {
+
+  app.get("/lp/create", function(req, res) {
+
+    // edited lp create to add in a lp_name
+  
+    db.lp.create({
+  
+      item_name: "fake stuff"
+  
+    })
+  
+      // pass the result of our call
+  
+      .then(function(dbLp) {
+  
+        // log the result to our terminal/bash window
+  
+        console.log(dbLp);
+  
+        // redirect
+  
+        res.redirect("/");
+  
+      });
+  
+  });
+
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
