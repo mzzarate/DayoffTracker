2168bb2f61ec857a4040bc2340d812dd7549173e
eleven_three_eleven <kimanikelly@users.noreply.github.com>
2019-03-21 20:52:02 -0400

    merge

--- models/schema.sql
+++ models/schema.sql
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 DROP DATABASE IF EXISTS example_db;
 CREATE DATABASE example_db;
-=======
--- DROP DATABASE IF EXISTS userInfo_db;
- CREATE DATABASE userInfo_db;
->>>>>>> origin/master
 
 -- DROP DATABASE IF EXISTS testdb;
 -- CREATE DATABASE testdb;
